5439c5e6f47891a17ace25dbe1e2243d7cd4ecd5
Ali Rasim Kocal <arkocal@gmail.com>
2014-01-31 16:03:19 +0200

    close #1 , close #2

--- dummy_arkocal.py
+++ dummy_arkocal.py
@@ -5,8 +5,13 @@ class dummyPlugin(plugins.Plugin):
         plugins.Plugin.__init__(self, manager)
         self.pname = "dummy_arkocal"
         self.addResponse("started", self.onStart)
-        
+        self.addResponse("change-dir", self.onChangeDir)
+        self.respondAfter["started"].append("dirTree")
     def onStart(self, signal, *args, **kwargs):
+        self.manager.raiseSignal("change-dir", 
+            newPath="/home/ali/Workspace/FileBrowser")
+
+    def onChangeDir(self, signal, *args, **kwargs):
         pass
 
 def createPlugin(manager):
